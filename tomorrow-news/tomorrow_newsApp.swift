fb4d87dbb697f78cf7ffa0cdc1fcdbc50d7acb9a
二哈快过来 <pettygadfly@gmail.com>
2020-12-23 14:55:44 +0800

    Initial Commit

---
+++ tomorrow-news/tomorrow_newsApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomorrow_newsApp.swift
+//  tomorrow-news
+//
+//  Created by silence on 2020/12/23.
+//
+
+import SwiftUI
+
+@main
+struct tomorrow_newsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
